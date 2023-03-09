2d2325f3e6d9c41593b0cb9a12d760fa10ea319f
ee0122ng <ee22011993hang@gmail.com>
2023-03-09 13:42:03 +0800

    initial commit

---
+++ paf-day01/src/main/java/iss/paf/pafday01/PafDay01Application.java
@@ -0,0 +1,13 @@
+package iss.paf.pafday01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafDay01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafDay01Application.class, args);
+	}
+
+}
---
+++ paf-day01/src/test/java/iss/paf/pafday01/PafDay01ApplicationTests.java
@@ -0,0 +1,13 @@
+package iss.paf.pafday01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafDay01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
