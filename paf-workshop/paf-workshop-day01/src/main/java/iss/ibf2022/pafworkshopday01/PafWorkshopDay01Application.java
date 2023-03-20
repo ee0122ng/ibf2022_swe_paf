ce4608fd80d9edbf68c3769bdaf299d47903c385
ee0122ng <ee22011993hang@gmail.com>
2023-03-20 17:32:37 +0800

    commit for workshop day01

---
+++ paf-workshop/paf-workshop-day01/src/main/java/iss/ibf2022/pafworkshopday01/PafWorkshopDay01Application.java
@@ -0,0 +1,13 @@
+package iss.ibf2022.pafworkshopday01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafWorkshopDay01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafWorkshopDay01Application.class, args);
+	}
+
+}
