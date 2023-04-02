1683f27550a42c8f724e6d9aa77ee5b5b294dae7
ee0122ng <ee22011993hang@gmail.com>
2023-04-02 11:34:56 +0800

    initial commit

---
+++ paf-workshop/paf-workshop-day08/src/main/java/iss/ibf2022/pafday08workshop/config/MongoConfig.java
@@ -0,0 +1,28 @@
+package iss.ibf2022.pafday08workshop.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+public class MongoConfig {
+
+    @Value("${mongo.url}")
+    private String url;
+
+    @Bean
+    @Qualifier("my-mongo")
+    public MongoTemplate createMongoTemplate() {
+        
+        MongoClient client = MongoClients.create(url);
+
+        // provide your database here
+        return new MongoTemplate(client, "games");
+    }
+    
+}
